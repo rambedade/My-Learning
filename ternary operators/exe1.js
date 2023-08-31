3ef063057bf51c763d1e50bf37cc4ddb14d96678
Rameshwar Bedade <rambedade2308@gmail.com>
2023-08-31 13:47:16 +0530

    --all

---
+++ ternary operators/exe1.js
@@ -0,0 +1,20 @@
+
+
+// Ternary Operator
+
+
+let name="ram"
+// if(name=="ram"){
+//       console.log("my name is Ram")
+// }else {
+//     console.log("my name is Bob")
+// }
+
+
+// condn ? statement1 : statement2
+// comparison ? true : false
+// here comparison is <= or = or >=
+
+let id=23
+name="shyam" && id==22 ? console.log("hey shyamya") : console.log("hey rama")
+
